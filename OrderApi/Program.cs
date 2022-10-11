22196abe2165ebf13a9c75e178fd79da52205090
kendeszabo <szabo.kende20@gmail.com>
2022-10-11 12:47:13 +0200

    Add existing project files to Git

---
+++ OrderApi/Program.cs
@@ -0,0 +1,61 @@
+using Microsoft.EntityFrameworkCore;
+using OrderApi.Data;
+using OrderApi.Infrastructure;
+using SharedModels;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// RabbitMQ connection string (I use CloudAMQP as a RabbitMQ server).
+// Remember to replace this connectionstring with your own.
+string cloudAMQPConnectionString =
+   "host=hawk.rmq.cloudamqp.com;virtualHost=aaqlhcqa;username=aaqlhcqa;password=dmojgvjNDOFGSV9WtVqJjcql0wnG6AtP";
+
+// Add services to the container.
+
+builder.Services.AddDbContext<OrderApiContext>(opt => opt.UseInMemoryDatabase("OrdersDb"));
+
+// Register repositories for dependency injection
+builder.Services.AddScoped<IRepository<Order>, OrderRepository>();
+
+// Register database initializer for dependency injection
+builder.Services.AddTransient<IDbInitializer, DbInitializer>();
+
+// Register MessagePublisher (a messaging gateway) for dependency injection
+builder.Services.AddSingleton<IMessagePublisher>(new
+    MessagePublisher(cloudAMQPConnectionString));
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// Initialize the database.
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var dbContext = services.GetService<OrderApiContext>();
+    var dbInitializer = services.GetService<IDbInitializer>();
+    dbInitializer.Initialize(dbContext);
+}
+
+// Create a message listener in a separate thread.
+Task.Factory.StartNew(() =>
+    new MessageListener(app.Services, cloudAMQPConnectionString).Start());
+
+//app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
